81faac781019d40e7e73eab5f2b6daa4b433dc8d
Brian Lawler <bplawler@gmail.com>
2020-10-11 00:09:48 -0600

    more bearch->dominion

--- api/src/main/scala/twita/dominion/api/ex/Exceptions.scala
+++ api/src/main/scala/twita/dominion/api/ex/Exceptions.scala
@@ -3,13 +3,13 @@ package twita.dominion.api.ex
 import play.api.libs.json.JsObject
 import play.api.libs.json.Json
 
-class BearchRuntimeException(msg: String) extends RuntimeException(msg)
+class DominionRuntimeException(msg: String) extends RuntimeException(msg)
 
 class ObjectDeleted[ObjectId](id: ObjectId)
-  extends BearchRuntimeException(s"id: ${id}")
+  extends DominionRuntimeException(s"id: ${id}")
 
 class ObjectUpdateNotApplied(obj: Object, discriminator: JsObject)
-  extends BearchRuntimeException(s"object ${obj.getClass.getName} discriminator ${Json.prettyPrint(discriminator)}")
+  extends DominionRuntimeException(s"object ${obj.getClass.getName} discriminator ${Json.prettyPrint(discriminator)}")
 
 class ObjectDoesNotMeetConstraints(q: JsObject, constraint: JsObject)
-  extends BearchRuntimeException(s"query: ${q.toString}, constraint: ${constraint.toString}")
+  extends DominionRuntimeException(s"query: ${q.toString}, constraint: ${constraint.toString}")
